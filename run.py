8f6d12050d78deb39c3f447d4e603bfb6ffaa0db
lily <lilyhzhang1029@gmail.com>
2019-03-24 18:45:53 -0400

    add multiprocessing

--- run.py
+++ run.py
@@ -5,6 +5,8 @@ import torch
 from datasets.load import load_toy_bars
 from training.train_vae import train
 
+import concurrent.futures
+
 # global params
 topic_init_and_fixed = True
 datadir = 'toy_bars'
@@ -27,14 +29,28 @@ inference_techniques = ['vae', 'svi', 'hmc']
 # 0. load data
 data_tr, data_va, data_te = load_toy_bars(datadir, vocab_size)
 
+def train_save_VAE(n_hidden_layers, n_hidden_units):
+    vae = mod.VAE_tf(n_hidden_layers=n_hidden_layers, n_hidden_units=n_hidden_units, n_topics=n_topics,
+                     vocab_size=vocab_size, tensorboard=True)
+    vae = train(data_tr, vae, training_epochs=100, tensorboard=True,
+                tensorboard_logs_dir='dump/logs_{}_{}'.format(n_hidden_layers, n_hidden_units))
+    vae.save()
+    vae.sess.close()
+    tf.reset_default_graph()
+
+
 # 1. train vae
-for n_hidden_layers in vae_params['n_hidden_layers']:
-    for n_hidden_units in vae_params['n_hidden_units']:
-        vae = mod.VAE_tf(n_hidden_layers=n_hidden_layers, n_hidden_units=n_hidden_units, n_topics=n_topics, vocab_size=vocab_size)
-        vae = train(data_tr, vae, training_epochs=100, tensorboard=True, tensorboard_logs_dir='logs_{}_{}'.format(n_hidden_layers, n_hidden_units))
-        vae.save()
-        vae.sess.close()
-        tf.reset_default_graph()
+with concurrent.futures.ProcessPoolExecutor() as executor:
+    for n_hidden_layers in vae_params['n_hidden_layers']:
+        for n_hidden_units in vae_params['n_hidden_units']:
+            future = executor.submit(train_save_VAE, n_hidden_layers, n_hidden_units)
+            print("Run with: %s | %s" % (n_hidden_layers, n_hidden_units))
+
+    for future in concurrent.futures.as_completed():
+            try:
+                future.result()
+            except Exception as exc:
+                print('Exception raised: %s' % (exc))
 
 # 2. infer from model
 sample_docs = torch.from_numpy(data_tr[:5].astype(np.float32))
