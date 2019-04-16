6997594c99f3972a47151bc27852fb42f6a7903f
lily <lilyhzhang1029@gmail.com>
2019-04-15 21:33:27 -0400

    prepare for big run

--- run_vae_hallucinations.py
+++ run_vae_hallucinations.py
@@ -10,10 +10,9 @@ from common import train_save_VAE, save_loglik_to_csv, save_reconstruction_array
 
 
 # global params
-datadir = 'toy_bars_10x10'
-results_dir = 'vae_run4'
+results_dir = 'mdreviews'
 results_file = 'results.csv'
-n_topics = 18
+n_topics = 20
 
 # vae params
 vae_params = {
@@ -22,10 +21,7 @@ vae_params = {
     'n_hidden_layers': [1, 2, 5],
     'n_hidden_units': [10, 20, 50, 100]
 }
-# vae_params = {
-#     'n_hidden_layers': [1, 2],
-#     'n_hidden_units': [10, 20, 50, 100]
-# }
+
 num_examples = 10
 sample_idx = list(range(10))
 
@@ -45,7 +41,7 @@ datasets[2] = datasets[2][:792]
 
 # various VAEs, all with fixed decoders
 init_params = {
-    'topic_init': 'resources/topics_10x10.npy',
+    'topic_init': 'resources/mdreviews_topics.npy',
     'topic_trainable': False,
     'vocab_size': 100,
     'n_topics': n_topics,
@@ -116,36 +112,29 @@ orig_model_configs = [
 #     lda_sym_fixed, lda_sym, lda_sym_fixed_with_scale, lda_sym_with_scale
 # ]
 
-# for model_config in orig_model_configs:
-#     for n_hidden_layers in vae_params['n_hidden_layers']:
-#         for n_hidden_units in vae_params['n_hidden_units']:
-#             model_config.update({
-#                 'n_hidden_layers': n_hidden_layers,
-#                 'n_hidden_units': n_hidden_units,
-#             })
-#             print(model_config)
-#             train_save_VAE(data_tr, model_config, hallucinations=False)
+for model_config in orig_model_configs:
+    for n_hidden_layers in vae_params['n_hidden_layers']:
+        for n_hidden_units in vae_params['n_hidden_units']:
+            model_config.update({
+                'n_hidden_layers': n_hidden_layers,
+                'n_hidden_units': n_hidden_units,
+            })
+            print(model_config)
+            train_save_VAE(data_tr, model_config, hallucinations=False)
 
-# model_configs = orig_model_configs[:]
-#
-# for model_config in model_configs:
-#     for n_hidden_layers in vae_params['n_hidden_layers']:
-#         for n_hidden_units in vae_params['n_hidden_units']:
-#             model_config.update({
-#                 'n_hidden_layers': n_hidden_layers,
-#                 'n_hidden_units': n_hidden_units,
-#                 'model_name': model_config['model_name'] + '_hallucinations'
-#             })
-#             print(model_config)
-#             train_save_VAE(data_tr, model_config, hallucinations=True)
-#
-# for model_config in model_configs_sym:
-#     for n_hidden_layers in vae_params['n_hidden_layers']:
-#         model_config.update({
-#             'n_hidden_layers': n_hidden_layers,
-#         })
-#         print(model_config)
-#         train_save_VAE(data_tr, model_config, hallucinations=True)
+model_configs = orig_model_configs[:]
+for model_config in model_configs:
+    model_config['model_name'] = model_config['model_name'] + '_hallucinations'
+
+for model_config in model_configs:
+    for n_hidden_layers in vae_params['n_hidden_layers']:
+        for n_hidden_units in vae_params['n_hidden_units']:
+            model_config.update({
+                'n_hidden_layers': n_hidden_layers,
+                'n_hidden_units': n_hidden_units,
+            })
+            print(model_config)
+            train_save_VAE(data_tr, model_config, hallucinations=True)
 
 for model_config in orig_model_configs:
     for n_hidden_layers in vae_params['n_hidden_layers']:
@@ -172,55 +161,28 @@ for model_config in orig_model_configs:
                 for i in range(10):
                     save_loglik_to_csv(data, vae.model, posterior, model_config, num_samples=10)
 
-# model_configs = orig_model_configs[:]
-# for model_config in model_configs:
-#     model_config['model_name'] = model_config['model_name'] + '_hallucinations'
-#
-# for model_config in model_configs:
-#     for n_hidden_layers in vae_params['n_hidden_layers']:
-#         for n_hidden_units in vae_params['n_hidden_units']:
-#             model_config.update({
-#                 'n_hidden_layers': n_hidden_layers,
-#                 'n_hidden_units': n_hidden_units,
-#             })
-#             vae = VAE_pyro(**model_config)
-#             state_dict = vae.load()
-#             vae.load_state_dict(state_dict)
-#             for data_name, data in zip(dataset_names, datasets):
-#                 # pyro scheduler doesn't have any effect in the VAE case since we never take any optimization steps
-#                 pyro_scheduler = StepLR(
-#                     {'optimizer': torch.optim.Adam, 'optim_args': {"lr": .1}, 'step_size': 10000, 'gamma': 0.95})
-#                 vae_svi = SVI(vae.model, vae.encoder_guide, pyro_scheduler, loss=Trace_ELBO(), num_steps=0)
-#                 data = torch.from_numpy(data.astype(np.float32))
-#                 posterior = vae_svi.run(data)
-#                 model_config.update({
-#                     'data_name': data_name
-#                 })
-#                 print(model_config)
-#                 save_reconstruction_array(vae, posterior, sample_idx, model_config)
-#                 for i in range(10):
-#                     save_loglik_to_csv(data, vae.model, posterior, model_config, num_samples=10)
 
-#
-# for model_config in model_configs_sym:
-#     for n_hidden_layers in vae_params['n_hidden_layers']:
-#         model_config.update({
-#             'n_hidden_layers': n_hidden_layers
-#         })
-#         vae = VAE_pyro(**model_config)
-#         state_dict = vae.load()
-#         vae.load_state_dict(state_dict)
-#         for data_name, data in zip(dataset_names, datasets):
-#             # pyro scheduler doesn't have any effect in the VAE case since we never take any optimization steps
-#             pyro_scheduler = StepLR(
-#                 {'optimizer': torch.optim.Adam, 'optim_args': {"lr": .1}, 'step_size': 10000, 'gamma': 0.95})
-#             vae_svi = SVI(vae.model, vae.encoder_guide, pyro_scheduler, loss=Trace_ELBO(), num_steps=0)
-#             data = torch.from_numpy(data.astype(np.float32))
-#             posterior = vae_svi.run(data)
-#             model_config.update({
-#                 'data_name': data_name
-#             })
-#             print(model_config)
-#             save_reconstruction_array(vae, posterior, sample_idx, model_config)
-#             for i in range(10):
-#                 save_loglik_to_csv(data, vae.model, posterior, model_config, num_samples=10)
+for model_config in model_configs:
+    for n_hidden_layers in vae_params['n_hidden_layers']:
+        for n_hidden_units in vae_params['n_hidden_units']:
+            model_config.update({
+                'n_hidden_layers': n_hidden_layers,
+                'n_hidden_units': n_hidden_units,
+            })
+            vae = VAE_pyro(**model_config)
+            state_dict = vae.load()
+            vae.load_state_dict(state_dict)
+            for data_name, data in zip(dataset_names, datasets):
+                # pyro scheduler doesn't have any effect in the VAE case since we never take any optimization steps
+                pyro_scheduler = StepLR(
+                    {'optimizer': torch.optim.Adam, 'optim_args': {"lr": .1}, 'step_size': 10000, 'gamma': 0.95})
+                vae_svi = SVI(vae.model, vae.encoder_guide, pyro_scheduler, loss=Trace_ELBO(), num_steps=0)
+                data = torch.from_numpy(data.astype(np.float32))
+                posterior = vae_svi.run(data)
+                model_config.update({
+                    'data_name': data_name
+                })
+                print(model_config)
+                save_reconstruction_array(vae, posterior, sample_idx, model_config)
+                for i in range(10):
+                    save_loglik_to_csv(data, vae.model, posterior, model_config, num_samples=10)
