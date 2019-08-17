2e011935cba3e0d0eae62fdaa469ffe47384a969
lily <lilyhzhang1029@gmail.com>
2019-08-17 17:30:40 -0400

    CLEAN: remove unnecessary files

--- plot_reconstruction_vs_sparsity.py
+++
@@ -1,8 +0,0 @@
-import seaborn as sns
-import pandas as pd
-
-
-df = pd.read_csv('mdreviews1/reconstruction_by_sparsity_scale.csv')
-g = sns.lineplot('num_topics', 'posterior_predictive_density', 'dataset', data=df)
-fig = g.get_figure()
-fig.savefig('mdreviews1/reconstruction_by_sparsity_scale.pdf')
