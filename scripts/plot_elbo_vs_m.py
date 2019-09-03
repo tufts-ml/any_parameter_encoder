0bbaca1ca79a617eb50cf3df91024fd9cee27648
lily <lilyhzhang1029@gmail.com>
2019-09-02 20:50:01 -0400

    ADD: plot elbo vs. m

---
+++ scripts/plot_elbo_vs_m.py
@@ -0,0 +1,21 @@
+import os
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import argparse
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Results summary')
+    parser.add_argument('results_dir', type=str, help='Experiment Directory')
+    args = parser.parse_args()
+
+    df = pd.read_csv(os.path.join(args.results_dir, 'elbo_vs_m.csv'))
+    df.dropna(inplace=True, axis=0)
+    df.elbo = df.elbo.astype(float)
+    df.m = df.m.astype(float)
+    print(df.head())
+    g = sns.FacetGrid(df, col='data')
+    g.map(sns.lineplot,'m', 'elbo', 'inference')
+    plt.savefig(os.path.join(args.results_dir, 'elbo_vs_m.pdf'))
