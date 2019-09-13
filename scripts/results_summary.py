bb6d25577cbece28dee847d1db02d720d982a892
lily <lilyhzhang1029@gmail.com>
2019-09-12 23:25:01 -0400

    ADD: normalized option for results summary

--- scripts/results_summary.py
+++ scripts/results_summary.py
@@ -1,6 +1,8 @@
 import os
 import pandas as pd
 import argparse
+import numpy as np
+from scripts.normalize_posterior_predictive import normalize_posterior_predictive
 
 
 if __name__ == "__main__":
@@ -10,12 +12,18 @@ if __name__ == "__main__":
     parser.add_argument('--normalized', help='Experiment Directory', action='store_true')
     args = parser.parse_args()
 
-    results_dir = 'naive_reconstructions'
     print(args.results_dir)
-    if args.normalized:
-        df = pd.read_csv(os.path.join(args.results_dir, 'results_normalized.csv'))
-    else:
-        df = pd.read_csv(os.path.join(args.results_dir, 'results.csv'))
+    df = pd.read_csv(os.path.join(args.results_dir, 'results.csv'), header=None)
     df.columns = ['inference', 'model', 'dataset', 'n_hidden_layers', 'n_hidden_units', 'posterior_predictive_density']
+    if args.normalized:
+        dataset_names = ['train', 'valid', 'test']
+        documents_file = os.path.join(args.results_dir, 'documents.npy')
+        if not os.path.exists(documents_file):
+            documents_file = os.path.join('experiments', 'documents.npy')
+        documents = np.load(documents_file)
+        # valid = np.load(os.path.join(results_dir, 'valid_documents.npy'))
+        datasets = [documents * 200, documents * 5, documents * 5]
+        df = normalize_posterior_predictive(df, datasets, dataset_names)
+        df.to_csv(os.path.join(args.results_dir, 'results_normalized.csv'), header=False, index=False)
     print(df.groupby(['dataset', 'inference']).posterior_predictive_density.agg(
         {'std':'std', 'mean':'mean', 'max': 'max'}))
