15b64c32f2a26cdbe0e00da1b843c5d435e882dc
lily <lilyhzhang1029@gmail.com>
2019-09-29 02:19:11 -0400

    ADD: save model checkpoints, get elbo for standard vae

--- common.py
+++ common.py
@@ -29,7 +29,7 @@ MIN_LOG_PROB = -9999999999
 
 def train_save_VAE(train_data, valid_data, model_config, training_epochs=120, batch_size=200,
                    tensorboard=True, hallucinations=False, shuffle=True, display_step=5, recreate_docs=True,
-                   vocab_size=100, n_topics=20):
+                   vocab_size=100, n_topics=20, save_iter=100):
     vae = VAE_tf(tensorboard=tensorboard, **model_config)
     tensorboard_logs_dir = os.path.join(
         model_config['results_dir'], model_config['model_name'],
@@ -40,7 +40,7 @@ def train_save_VAE(train_data, valid_data, model_config, training_epochs=120, ba
             n_topics=n_topics)
     else:
         vae = train(train_data, valid_data, vae, training_epochs=training_epochs, tensorboard=tensorboard, batch_size=batch_size,
-            tensorboard_logs_dir=tensorboard_logs_dir, results_dir=model_config['results_dir'], display_step=display_step)
+            tensorboard_logs_dir=tensorboard_logs_dir, results_dir=model_config['results_dir'], display_step=display_step, save_iter=save_iter)
     if recreate_docs:
         for data_name, batch_xs in zip(['train', 'valid'], [train_data[:10], valid_data[:10]]):
             recreated_docs, _, _ = vae.recreate_input(batch_xs)
@@ -136,7 +136,7 @@ def get_elbo_vs_m(vae, dataset_names, datasets, results_dir, distances):
                 pyro.clear_param_store()
 
 
-def get_elbo_csv(vae, results_dir, restart=True):
+def get_elbo_csv(vae, vae_single, results_dir, restart=True):
     dataset_names = ['train', 'valid', 'test']
     try:
         train_topics = np.load(os.path.join(results_dir, 'train_topics.npy'))
@@ -159,7 +159,7 @@ def get_elbo_csv(vae, results_dir, restart=True):
     num_docs = len(documents)
     with open(os.path.join(results_dir, 'elbos.csv'), 'w') as f:
         writer = csv.writer(f, delimiter=',')
-        writer.writerow(['Dataset', 'Topic index', 'SVI ELBO', 'VAE encoder ELBO'])
+        writer.writerow(['Dataset', 'Topic index', 'SVI ELBO', 'Decoder-aware encoder ELBO', 'Standard encoder ELBO'])
         for data_name, topics_and_data in zip(dataset_names, datasets):
             topics, data = unzip_X_and_topics(topics_and_data)
             data = torch.from_numpy(data.astype(np.float32))
@@ -172,16 +172,22 @@ def get_elbo_csv(vae, results_dir, restart=True):
                 topics_i = topics[i * num_docs: (i + 1) * num_docs]
                 vae_elbo = Trace_ELBO()
                 vae_svi = SVI(vae.model, vae.encoder_guide, pyro_scheduler, loss=vae_elbo, num_steps=0, num_samples=100)
-
                 vae_posterior = vae_svi.run(data_i, topics_i)
                 vae_loss = -vae_svi.evaluate_loss(data_i, topics_i)
+
+                vae_single_svi = SVI(vae.model, vae.encoder_guide, pyro_scheduler, loss=vae_elbo, num_steps=0, num_samples=100)
+                vae_single_posterior = vae_single_svi.run(data_i, topics_i)
+                vae_single_loss = -vae_single_svi.evaluate_loss(data_i, topics_i)
+
                 svi_loss = np.nan
                 if restart:
-                    while torch_isnan(svi_loss):
+                    i = 0
+                    while torch_isnan(svi_loss) and i < 3:
                         svi_elbo = Trace_ELBO()
                         svi = SVI(vae.model, vae.mean_field_guide, pyro_scheduler, loss=svi_elbo, num_steps=400, num_samples=100)
                         svi_posterior = svi.run(data_i, topics_i)
                         svi_loss = -svi.evaluate_loss(data_i, topics_i)
+                        i += 1
                         pyro.clear_param_store()
                 else:
                     svi_elbo = Trace_ELBO()
@@ -189,7 +195,7 @@ def get_elbo_csv(vae, results_dir, restart=True):
                     svi_posterior = svi.run(data_i, topics_i)
                     svi_loss = -svi.evaluate_loss(data_i, topics_i)
                     pyro.clear_param_store()
-                writer.writerow([data_name, i, svi_loss, vae_loss])
+                writer.writerow([data_name, i, svi_loss, vae_loss, vae_single_loss])
                 pyro.clear_param_store()
 
 
--- visualization/ranking.py
+++ visualization/ranking.py
@@ -6,5 +6,9 @@ import seaborn as sns
 def plot_svi_vs_vae_elbo(results_dir):
     df = pd.read_csv(os.path.join(results_dir, 'elbos.csv'))
     g = sns.FacetGrid(df, col="Dataset")
-    g.map(plt.scatter, "VAE encoder ELBO", "SVI ELBO").add_legend()
+    g.map(plt.scatter, "Decoder-aware encoder ELBO", "SVI ELBO").add_legend()
     plt.savefig(os.path.join(results_dir, 'elbo_ranking.png'))
+    plt.close()
+    g = sns.FacetGrid(df, col="Dataset")
+    g.map(plt.scatter, "Standard encoder ELBO", "SVI ELBO").add_legend()
+    plt.savefig(os.path.join(results_dir, 'elbo_ranking_single.png'))
