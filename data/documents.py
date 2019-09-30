237ea737e1f7fbac211617436834c19f07c986c4
lily <lilyhzhang1029@gmail.com>
2019-09-30 00:32:08 -0400

    ADD: document generations takes num words and num topics into account

--- data/documents.py
+++ data/documents.py
@@ -5,18 +5,18 @@ from visualization.reconstructions import plot_side_by_side_docs
 from utils import normalize1d
 
 
-def generate_documents(topics, num_docs, alpha=.05, seed=0):
+def generate_documents(topics, num_docs, num_topics, vocab_size, alpha=.05, seed=0):
     np.random.seed(seed)
-    doc_topic_dists = np.random.dirichlet([alpha] * 18, size=num_docs)
+    doc_topic_dists = np.random.dirichlet([alpha] * num_topics, size=num_docs)
     documents = []
     for pi in doc_topic_dists:
         num_words = np.random.poisson(50)
         # word_probs = normalize1d(np.mean([a * np.array(b) for a, b in zip(pi, topics)], axis=0))
         # doc = np.random.multinomial(num_words, word_probs)
         
-        doc = np.zeros(100)
+        doc = np.zeros(vocab_size)
         for _ in range(num_words):
-            z = np.random.choice(range(18), p=pi)
+            z = np.random.choice(range(num_topics), p=pi)
             doc += np.random.multinomial(1, topics[z])
         documents.append(doc.astype(np.float32))
     return documents, doc_topic_dists
