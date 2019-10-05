6cf74bb3fa0206ee4c77ced5409379341a5bf024
lily <lilyhzhang1029@gmail.com>
2019-10-05 00:18:13 -0400

    ADD: always print valid cost even if no tensorboard

--- training/train_vae.py
+++ training/train_vae.py
@@ -145,6 +145,13 @@ def train(
             
             # Display logs per epoch step
             if num_steps % display_step == 0:
+                if vae_meta:
+                    valid_cost = vae.sess.run(vae.cost,
+                                            feed_dict={vae.x: X_val, vae.topics: topics_val, vae.keep_prob: 1.0})
+                else:
+                    valid_cost = vae.sess.run(vae.cost,
+                                            feed_dict={vae.x: X_val, vae.keep_prob: 1.0})
+                logger.info('valid summary: {}'.format(valid_cost))
                 if tensorboard:
                     merge = tf.summary.merge(vae.summaries)
                     if vae_meta:
@@ -155,15 +162,7 @@ def train(
                                                 feed_dict={vae.x: X, vae.keep_prob: 1.0})
                     train_writer.add_summary(summary, epoch)
                     if plot_valid_cost:
-                        if vae_meta:
-                            valid_cost = vae.sess.run(vae.cost,
-                                                    feed_dict={vae.x: X_val, vae.topics: topics_val, vae.keep_prob: 1.0})
-                        else:
-                            valid_cost = vae.sess.run(vae.cost,
-                                                    feed_dict={vae.x: X_val, vae.keep_prob: 1.0})
-                        print(valid_cost)
                         print('writing valid summary')
-                        logger.info('writing valid summary: {}'.format(valid_cost))
                         valid_summary = tf.Summary(value=[tf.Summary.Value(tag="valid_loss", simple_value=valid_cost)])
                         valid_writer.add_summary(valid_summary, epoch)
                         valid_writer.flush()
