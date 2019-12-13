e47a2d7b3f4de64fbb390e17bbe3c6adf83623c4
lily <lilyhzhang1029@gmail.com>
2019-12-13 01:09:10 -0500

    ADD: evaluation

--- run.py
+++ run.py
@@ -1,17 +1,25 @@
 import os
 import argparse
+import numpy as np
+import itertools
 import torch
 from torch.utils import data
-from pyro.optim import ExponentialLR
-
-from pyro.infer import SVI, Trace_ELBO
-from pyro.infer.mcmc import MCMC, NUTS
+import pyro
+from pyro.optim import ExponentialLR, StepLR
+from pyro.infer import Trace_ELBO
+from pyro.infer.mcmc import NUTS
 
 from dataset import ToyBarsDataset
 from model import VAE
 from train import train
+from evaluate import TimedSVI, TimedMCMC, TimedAVI
+from evaluate import get_posterior_predictive_density
 
 import wandb
+
+torch.manual_seed(0)
+pyro.set_rng_seed(0)
+np.random.seed(0)
 
 from multiprocessing import set_start_method
 try:
@@ -22,6 +30,10 @@ except RuntimeError:
 parser = argparse.ArgumentParser(description='Results summary')
 parser.add_argument('--results_dir', type=str, help='directory of results')
 parser.add_argument('--architecture', type=str, help='encoder architecture')
+parser.add_argument('--run_avi', help='run amortized variational inference', action='store_true')
+parser.add_argument('--run_svi', help='run SVI', action='store_true')
+parser.add_argument('--run_mcmc', help='run MCMC', action='store_true')
+parser.add_argument('--warmstart_mcmc', help='warmstart MCMC', action='store_true')
 args = parser.parse_args()
 
 use_cuda = torch.cuda.is_available()
@@ -60,28 +72,81 @@ train_config = {
     'results_dir': args.results_dir,
 }
 
+eval_config = {
+    'documents': 'data/toy_bar_docs.npy',
+    'topics': 'data/test_topics.npy'
+}
+
 if __name__ == "__main__":
     wandb.init(sync_tensorboard=True, project="any_parameter_encoder", entity="lily", name=args.results_dir)
+    names = []
+    inferences = []
+
     vae = VAE(**model_config)
-    model_path = os.path.join(args.results_dir, 'ape.dict')
-    if os.path.exists(model_path):
-        vae.load_state_dict(torch.load(model_path))
 
-    pyro_scheduler = ExponentialLR({'optimizer': torch.optim.Adam, 'optim_args': {"lr": .01}, 'gamma': 0.95})
-    vae_svi = SVI(vae.model, vae.encoder_guide, pyro_scheduler, loss=Trace_ELBO(), num_samples=100)
-    training_set = ToyBarsDataset(training=True, topics_file='data/train_topics.npy', num_models=50000, **data_config)
-    validation_set = ToyBarsDataset(training=False, topics_file='data/valid_topics.npy', num_models=500, **data_config)
-    training_generator = data.DataLoader(training_set, **loader_config)
-    validation_generator = data.DataLoader(validation_set, **loader_config)
-    vae_svi = train(vae_svi, training_generator, validation_generator, **train_config)
+    if args.run_avi:
+        model_path = os.path.join(args.results_dir, 'ape.dict')
+        if os.path.exists(model_path):
+            vae.load_state_dict(torch.load(model_path))
 
-    # we only save the model to use in downstream inference
-    torch.save(vae.state_dict(), model_path)
+        pyro_scheduler = ExponentialLR({'optimizer': torch.optim.Adam, 'optim_args': {"lr": .01}, 'gamma': 0.95})
+        avi = TimedAVI(vae.model, vae.encoder_guide, pyro_scheduler, loss=Trace_ELBO(), num_samples=100)
+        training_set = ToyBarsDataset(training=True, topics_file='data/train_topics.npy', num_models=50000, **data_config)
+        validation_set = ToyBarsDataset(training=False, topics_file='data/valid_topics.npy', num_models=500, **data_config)
+        training_generator = data.DataLoader(training_set, **loader_config)
+        validation_generator = data.DataLoader(validation_set, **loader_config)
+        avi = train(avi, training_generator, validation_generator, **train_config)
 
-    svi = SVI(vae_svi.model, vae.mean_field_guide, pyro_scheduler, loss=Trace_ELBO(), num_samples=100)
-    nuts_kernel = NUTS(vae.model, adapt_step_size=True)
-    mcmc = MCMC(nuts_kernel, num_samples=100, warmup_steps=100)
+        # we only save the model to use in downstream inference
+        torch.save(vae.state_dict(), model_path)
 
-    # evaluate_likelihood(vae_svi)
-    # evaluate_likelihood(svi)
-    # evaluate_likelihood(mcmc)
+        names.append('avi')
+        inferences.append(avi)
+
+    if args.run_svi:
+        # hyperparameters have been optimized
+        pyro_scheduler = StepLR({'optimizer': torch.optim.Adam, 'optim_args': {"lr": .05}, 'step_size': 4000, 'gamma': 0.95})
+        print(pyro_scheduler)
+        svi = TimedSVI(vae.model, vae.mean_field_guide, pyro_scheduler, loss=Trace_ELBO(), num_samples=100) #, num_steps=100000)
+        training_set = ToyBarsDataset(training=True, topics_file='data/test_topics.npy', num_models=10, **data_config)
+        training_generator = data.DataLoader(training_set, batch_size=500)
+        n_epochs = 10000
+        svi = train(svi, training_generator, training_generator, **{'epochs': n_epochs, 'use_cuda': use_cuda, 'results_dir': args.results_dir})
+        print(n_epochs)
+        names.append('svi')
+        inferences.append(svi)
+
+    if args.run_mcmc:
+        nuts_kernel = NUTS(vae.model, adapt_step_size=True)
+        mcmc = TimedMCMC(nuts_kernel, num_samples=100, warmup_steps=100)
+        names.append('mcmc')
+        inferences.append(mcmc)
+
+    dtype = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor
+    all_docs = np.load(eval_config['documents'])
+    all_topics = np.load(eval_config['topics'])
+    # doc_idx = np.random.choice(range(len(all_docs)), size=300)
+    # topic_idx = np.random.choice(range(len(all_topics)), size=300)
+    # documents = all_docs[doc_idx]
+    # topics = all_topics[topic_idx]
+    documents, topics = zip(*[combination for combination in itertools.product(all_docs, all_topics)])
+    documents = np.array(documents)
+    num_words = documents.sum()
+    print('num_words', num_words)
+    documents = torch.from_numpy(documents).type(dtype)
+    topics = torch.from_numpy(np.array(topics)).type(dtype)
+
+    for name, inference in zip(names, inferences):
+        if args.warmstart_mcmc and isinstance(inference, TimedMCMC):
+            # we re-initialize MCMC to use SVI posterior as a warm-start
+            nuts_kernel.initial_trace = svi.exec_traces[-1]
+            inference = TimedMCMC(nuts_kernel, num_samples=100, warmup_steps=100)
+        posterior = inference.run(documents, topics)
+        likelihoods = []
+        for _ in range(10):
+            likelihood = get_posterior_predictive_density(documents, topics, vae.model, posterior)
+            print('likelihood', likelihood)
+            likelihoods.append(likelihood / num_words)
+        print(name)
+        print(np.mean(posterior.run_times), np.std(posterior.run_times))
+        print(np.mean(likelihoods), np.std(likelihoods))
