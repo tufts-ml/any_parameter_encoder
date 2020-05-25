3ee8f73351523fd728f640502f86848a0af1b88a
lily <lilyhzhang1029@gmail.com>
2020-05-25 12:50:24 -0400

    ADD: argparse and optional adding architecture

--- no_training_metrics.py
+++ no_training_metrics.py
@@ -1,8 +1,14 @@
 import numpy as np
+import argparse
 import pandas as pd
 
-df = pd.read_csv('no_training_non_toy_bars.csv')
-architectures = ['template', 'template_unnorm', 'template_scaled', 'pseudo_inverse', 'pseudo_inverse_unnorm', 'pseudo_inverse_scaled']
-df['architecture'] = np.tile(np.repeat(architectures, 4), int(len(df) / (len(architectures) * 4)))
+
+parser = argparse.ArgumentParser(description='Results summary')
+parser.add_argument('--csv', type=str)
+args = parser.parse_args()
+df = pd.read_csv(args.csv)
+print(df.columns)
+if 'architecture' not in list(df.columns):
+    architectures = ['template', 'template_unnorm', 'template_scaled', 'pseudo_inverse', 'pseudo_inverse_unnorm', 'pseudo_inverse_scaled']
+    df['architecture'] = np.tile(np.repeat(architectures, 4), int(len(df) / (len(architectures) * 4)))
 print(df[df.model_type == 'avitm'].groupby(['metric', 'data_size', 'architecture']).loss.agg(mean=np.mean, std=np.std))
-print(df.head(10))
