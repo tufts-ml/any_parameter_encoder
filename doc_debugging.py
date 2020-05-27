FIX: variational variance param closer to 1

doc_debugging.py
@@ -151,7 +151,9 @@ if __name__ == "__main__":
         print(np.mean(likelihoods), np.std(likelihoods))
 
         print(pyro.get_param_store().get_all_param_names())
-        z_loc = pyro.get_param_store().get_param('z_loc')[0]
-        z_scale = pyro.get_param_store().get_param('z_scale')[0]
-        print(z_loc)
-        print(z_scale)
+        z_locs = pyro.get_param_store().get_param('z_loc')
+        z_scales = pyro.get_param_store().get_param('z_scale')
+        print(z_locs)
+        print(z_scales)
+        np.save(trace_filename.replace('.npy', '_loc.npy'), np.array(z_locs))
+        np.save(trace_filename.replace('.npy', '_scale.npy'), np.array(z_scales))

model.py
@@ -67,7 +67,7 @@ class Encoder(nn.Module):
             self.fcmu.weight.data = torch.eye(n_topics).data
             self.fcsigma.weight.data = torch.zeros((n_hidden_units, n_topics)).data
             self.fcmu.bias.data = torch.zeros(n_topics).data
-            self.fcsigma.bias.data = torch.ones(n_topics).data
+            self.fcsigma.bias.data = torch.ones(n_topics).data * .001  # gives a z_scale close to 1
 
         self.bnmu = nn.BatchNorm1d(n_topics)
         self.bnsigma = nn.BatchNorm1d(n_topics)