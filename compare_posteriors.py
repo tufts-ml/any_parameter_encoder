3b2aa1ec0c93b7ebaadd9150f212888648621ff0
lily <lilyhzhang1029@gmail.com>
2020-06-01 11:40:20 -0400

    UPDATE: compare posteriors

--- compare_posteriors.py
+++ compare_posteriors.py
@@ -2,9 +2,20 @@ import os
 import numpy as np
 from utils import softmax
 
+
 directory = 'data/toy_bars'
 true = np.load(os.path.join(directory, 'docs_many_words_dist.npy'))[:5]
 svi =  softmax(np.load(os.path.join(directory, 'svi_prior_loc.npy')))
 avi =  softmax(np.load(os.path.join(directory, 'avi_template_loc.npy')))
 prior =  softmax(np.load(os.path.join(directory, 'avi_prior_loc.npy')))
+docs = np.load(os.path.join(directory, 'docs_many_words.npy'))[:5]
+topics = np.load(os.path.join(directory, 'topics_many_words.npy'))
+k = np.matmul(docs, np.transpose(topics, (0, 2, 1)))
+i = 0
+def get_top_n(idx):
+    print('true', np.argpartition(true[idx], 4)[-4:])
+    print('svi', np.argpartition(svi[idx], 4)[-4:])
+    print('avi', np.argpartition(avi[idx], 4)[-4:])
+    print('k', np.argpartition(k[idx], 4)[-4:])
+print(get_top_n(i))
 import ipdb; ipdb.set_trace()
