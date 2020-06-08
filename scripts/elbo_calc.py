2c617b0c89b47b8d6311608816d97f9f0decd8de
lily <lilyhzhang1029@gmail.com>
2020-06-07 22:53:44 -0400

    FIX: multinomial pdf based on num words in doc, which varies

--- scripts/elbo_calc.py
+++ scripts/elbo_calc.py
@@ -93,7 +93,7 @@ def calc_elbo_per_token(x_DV, alpha_K, theta_KV, pi_DK, sigma, n_mc_samples=500,
 
         for d in range(D):
             log_prior_pdf[d] = np.sum(scipy.stats.norm(prior_loc_K, prior_stddev_K).logpdf(h_samp_DK[d]))
-            log_lik_pdf[d] = scipy.stats.multinomial(n=n_words_per_doc, p=np.dot(pi_samp_DK[d], theta_KV)).logpmf(x_DV[d])
+            log_lik_pdf[d] = scipy.stats.multinomial(n=sum(x_DV[0]), p=np.dot(pi_samp_DK[d], theta_KV)).logpmf(x_DV[d])
             log_q_pdf[d] = np.sum(scipy.stats.norm(loc_DK[d], scale_DK[d]).logpdf(h_samp_DK[d]))
 
         elbo_per_token = np.sum(log_prior_pdf + log_lik_pdf - log_q_pdf) / np.sum(x_DV)
@@ -124,5 +124,5 @@ if __name__ == "__main__":
     phat_V = phat_V / np.sum(phat_V)
     unigram_logpmf_pertoken = 0.0
     for d in range(n_docs):
-        unigram_logpmf_pertoken += scipy.stats.multinomial(n=n_words_per_doc, p=phat_V).logpmf(x_DV[d]) / np.sum(x_DV)
+        unigram_logpmf_pertoken += scipy.stats.multinomial(n=sum(x_DV[d]), p=phat_V).logpmf(x_DV[d]) / np.sum(x_DV)
     print(np.asarray([unigram_logpmf_pertoken]))
