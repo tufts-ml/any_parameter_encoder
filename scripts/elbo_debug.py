3170d8aa3a1ffcebe7934252e487da0e3d6bba5a
lily <lilyhzhang1029@gmail.com>
2020-06-13 22:31:48 -0400

    UPDATE: make sure pi sums to 1

--- scripts/elbo_debug.py
+++ scripts/elbo_debug.py
@@ -1,12 +1,15 @@
 import numpy as np
 from scripts.elbo_calc import calc_elbo_per_token
+from utils import softmax
 
 
 if __name__ == "__main__":
-    pi_DK = np.load('data/toy_bars1/avi_template_loc.npy')
+    pi_DK = softmax(np.load('data/toy_bars1/avi_template_loc.npy'))
     x_DV = np.load('data/toy_bars1/docs_many_words.npy')[:5]
     theta_KV = np.load('data/toy_bars1/topics_many_words.npy')
     alpha_K = np.ones(20) * .1
     sigma = .1
 
-    print(calc_elbo_per_token(x_DV, alpha_K, theta_KV, pi_DK, sigma, n_mc_samples=500, seed=0))
+    elbo_per_token_list = calc_elbo_per_token(x_DV, alpha_K, theta_KV, pi_DK, sigma, n_mc_samples=500, seed=0)
+    print(elbo_per_token_list)
+    print(sum(elbo_per_token_list))
